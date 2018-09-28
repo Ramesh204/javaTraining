e81746f4ba2a6e292122aca769da9da281b6df08
AC31856 <AC31856@SGH526RPRC.ctl.intranet>
2018-09-28 14:14:33 +0530

    WebApplication

--- ParseExample/src/com/training/Parser.java
+++ ParseExample/src/com/training/Parser.java
@@ -6,33 +6,46 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class Parser extends DefaultHandler {
 
+	boolean element = true;
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
 		
-		super.characters(ch, start, length);
+//		super.characters(ch, start, length);
+		
+		
+		
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
 		
-		super.endDocument();
+		//super.endDocument();
+		System.out.println("parsing ends");
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		
-		super.endElement(uri, localName, qName);
+		//super.endElement(uri, localName, qName);
+		System.out.println(localName);
 	}
 
 	@Override
 	public void startDocument() throws SAXException {
-				super.startDocument();
+				//super.startDocument();
+				System.out.println("parsing starts");
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 	
-		super.startElement(uri, localName, qName, attributes);
+		//super.startElement(uri, localName, qName, attributes);
+		
+		System.out.println(uri);
+		
+		
+		
+		
 	}
 
 	
